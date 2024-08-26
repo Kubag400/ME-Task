de4f77fcb11a2c23553d28038aa45b3b7d951194
Jakub Gawęda <kubag98@hotmail.com>
2024-08-26 09:40:43 +0200

    initial

---
+++ Server/Server/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+        [HttpGet("/Index")]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
